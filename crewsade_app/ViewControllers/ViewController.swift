88e762c3e9fb7debb4be5777a37112a9e445e6ea
Hugo Lefrant <hugo.lefrant@gmail.com>
2020-04-29 10:56:19 +0200

    Feat: Pod Install

---
+++ crewsade_app/ViewControllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  crewsade_app
+//
+//  Created by Lou Batier on 20/04/2020.
+//  Copyright © 2020 Lou Batier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("init")
+        print("test")
+    }
+
+
+}
+
