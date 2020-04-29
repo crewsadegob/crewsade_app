1e993e37b1f33b8c91006128fe132af2ad240bc5
Hugo Lefrant <hugo.lefrant@gmail.com>
2020-04-29 17:56:05 +0200

    Feature: Add TabBar Controller & Navigation controller

--- crewsade_app/ViewControllers/ViewController.swift
+++ crewsade_app/ViewControllers/ViewController.swift
@@ -13,10 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("init")
-        print("test")
+      
     }
-
-
 }
 
