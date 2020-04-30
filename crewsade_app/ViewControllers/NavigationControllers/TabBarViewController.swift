0e08a77656112d796a36d8017fdf72504583b114
Hugo Lefrant <hugo.lefrant@gmail.com>
2020-04-30 14:10:10 +0200

    Feature: Center button

--- crewsade_app/ViewControllers/NavigationControllers/TabBarViewController.swift
+++ crewsade_app/ViewControllers/NavigationControllers/TabBarViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
+    @IBOutlet weak var TabBar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addCenterButton()
     }
-    
+
+    private func addCenterButton() {
+        let button = UIButton(type: .custom)
+        let widthTabBar = TabBar.frame.size.width
+           
+        button.frame = CGRect(x: (widthTabBar / 2) - 30, y: -30, width: 60, height: 60)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Polly-Regular",size: 45)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.backgroundColor = UIColor.CrewSade.mainColor
+
+        TabBar.addSubview(button)
+       // view.bringSubviewToFront(button)
+
+        button.addTarget(self, action: #selector(didTouchCenterButton(_:)), for: .touchUpInside)
+    }
+
+    @objc private func didTouchCenterButton(_ sender: AnyObject) {
+        print("!!")
+    }
+ 
 
     /*
     // MARK: - Navigation
