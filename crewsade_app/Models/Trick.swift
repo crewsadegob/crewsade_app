f3cfb6c42b391661679ef0b5cc46ecc902512bb2
Hugo Lefrant <hugo.lefrant@gmail.com>
2020-05-04 10:32:24 +0200

    Feature: Model User

--- crewsade_app/Models/Trick.swift
+++ crewsade_app/Models/Trick.swift
@@ -9,17 +9,11 @@
 import Foundation
 import FirebaseFirestore
 
-class Trick {
+struct Trick {
     
     var name:String
     var content:String
     var level:String?
     var reference:DocumentReference
     
-    init(name:String, content:String, level:String?, reference: DocumentReference) {
-        self.name = name
-        self.content = content
-        self.level = level
-        self.reference = reference
-    }
 }
---
+++ crewsade_app/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  crewsade_app
+//
+//  Created by Hugo Lefrant on 04/05/2020.
+//  Copyright © 2020 Lou Batier. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let username: String?
+    let firstname: String?
+    let lastname: String?
+    var tricks: [Trick]
+}
